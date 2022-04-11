5a368ad2a629e02245bb5eada5e082e73d65f0ca
Eng-Janvier <nijas2012@yahoo.com>
2022-04-11 11:27:06 +0200

    Add

--- mysql_api.py
+++ mysql_api.py
@@ -3,13 +3,13 @@ import mysql.connector as mysql
 App = Flask(__name__)
 App.config['DEBUG'] = True
 
-db = mysql.connect(
-    host = "localhost",
-    user = "root",
-    passwd = "",
-    database = "flask_api"
-)
-cursor = db.cursor()
+# db = mysql.connect(
+#     host = "localhost",
+#     user = "root",
+#     passwd = "",
+#     database = "flask_api"
+# )
+# cursor = db.cursor()
 
 @App.route('/insert/dht/data', methods = ['GET'])
 def api_data():
@@ -20,8 +20,9 @@ def api_data():
         hum = float(request.args['Humidity'])
         querry = "INSERT INTO dht_api (TEMPERATURE,HUMIDITY) VALUES (%s,%s)"
         values =(temp,hum)
-        cursor.execute(querry,values)
-        db.commit()
+        # cursor.execute(querry,values)
+        # db.commit()
+        return "Hello World, This Nijas"
     else:
         return "<h1> No data entries provided,</h1><p> Please provide a list of data to be inserted.</p>"
 
